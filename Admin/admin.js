1c29c994307fb1b527e187960be50f2a03e12e34
quocanhvn <quocanhdohuu@gmail.com>
2025-10-19 20:22:55 +0700

    tao khung giao dien

---
+++ Admin/admin.js
@@ -0,0 +1,16 @@
+// Xử lý chuyển trang khi bấm menu
+const buttons = document.querySelectorAll(".menu-btn");
+const pages = document.querySelectorAll(".page");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const target = btn.getAttribute("data-target");
+    pages.forEach((p) => {
+      p.classList.remove("active");
+      if (p.id === target) p.classList.add("active");
+    });
+  });
+});
