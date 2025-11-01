8405a12a9a6ec3eb05527a673e1be7d719c494ad
hieuhoang117 <hieuhoangminh2403@gmail.com>
2025-11-01 16:20:51 +0000

    Bổ sung chức năng thêm ghi chú sức khỏe và dinh dưỡng, cải thiện giao diện và xử lý sự kiện cho các phần liên quan đến sức khỏe.

--- Giaovien/jvGV.js
+++ Giaovien/jvGV.js
@@ -77,9 +77,6 @@ function doiLL(trang1,trang2,trang3){
     document.querySelector(trang1).style.display='block';
     document.querySelector(trang2).style.display='none';
     document.querySelector(trang3).style.display='none';
-    document.querySelector(trang4).style.display='none';
-    document.querySelector(trang5).style.display='none';
-
 }
 btnHopThuDenLL.addEventListener('click',function(){
     anLL(btnHopThuDenLL);
@@ -93,3 +90,73 @@ btnDanhBaLL.addEventListener('click',function(){
     anLL(btnDanhBaLL);
     doiLL('#DanhBaLL','#HopThuDenLL','#DaGuiLL');
 });
+
+//---------------------------------Sức khỏe------------------------------------
+const btnThemGhiChuSK=document.querySelector('#ThemGhichuSK');
+const btnThoatCuaSoTGCSK=document.querySelector('#btbThoatcsoGCSK');
+const btntheGCCSSK=document.querySelector('#btncuasoTGCSK');
+btnThemGhiChuSK.addEventListener('click',function(){
+    document.querySelector('#CuaSoThemGhiChuSK').style.display='block';
+});
+btnThoatCuaSoTGCSK.addEventListener('click',function(){
+    document.querySelector('#CuaSoThemGhiChuSK').style.display='none';
+});
+btntheGCCSSK.addEventListener('click',function(){
+    alert('Thêm ghi chú sức khỏe thành công!');
+    document.querySelector('#CuaSoThemGhiChuSK').style.display='none';
+    if(document.getElementById('chonHocSinhGhiChuSK').value===""){
+        alert('Vui lòng chọn học sinh!');
+        return;
+    }else{
+        themghichuvaobang();
+    }
+    
+});
+
+function themghichuvaobang(){
+    let table=document.getElementById('DanhSachSK');
+    let row=table.insertRow(-1);
+
+    let cell1=row.insertCell(0);
+    let cell2=row.insertCell(1);
+    let cell3=row.insertCell(2);
+    let cell4=row.insertCell(3);
+    let cell5=row.insertCell(4);
+    let cell6=row.insertCell(5);
+    let cell7=row.insertCell(6);
+
+    let tenHS = document.getElementById('chonHocSinhGhiChuSK').value;
+    let tinhTrangSK = document.querySelector('input[name="suckhoe"]:checked')?.value || "";
+    let ghiChuSK = document.getElementById('GhichuveTTSK').value;
+    let cheDoAn = document.querySelector('input[name="BuaAN"]:checked')?.value || "";
+    let ghiChuDD = document.getElementById('GhichuveĐSK').value;
+    let buaAn = document.getElementById("ckbuaanSK").checked ;
+
+    let cda="";
+    if(cheDoAn==="Bình thường"){
+        cda=`<span class="CheDoAnSK">${cheDoAn}</span>`;
+    }else if(cheDoAn==="Dị Ứng"){
+        cda=`<span class="CheDoAnSK2">${cheDoAn}</span>`;
+    }else if(cheDoAn==="Chế độ ăn đặc biệt"){
+        cda=`<span class="CheDoAnSK3">${cheDoAn}</span>`;
+    }
+     let ttsk="";
+    if(tinhTrangSK==="Khỏe mạnh"){
+        ttsk=`<span class="TinhtrangSK">${tinhTrangSK}</span>`;
+    }else if(tinhTrangSK==="Ốm"){
+        ttsk=`<span class="TinhtrangSK2">${tinhTrangSK}</span>`;
+    }else if(tinhTrangSK==="Chấn Thương"){
+        ttsk=`<span class="TinhtrangSK3">${tinhTrangSK}</span>`;
+    }else if(tinhTrangSK==="Đang dùng thuốc"){
+        ttsk=`<span class="TinhtrangSK4">${tinhTrangSK}</span>`;
+    }
+    cell1.innerHTML =`<span">${tenHS}</span><br><span style="color: gray;">HS001</span>`;
+    cell2.innerHTML = ttsk;
+    cell3.innerHTML = ghiChuSK;
+    cell4.innerHTML = cda;
+    cell5.innerHTML = ghiChuDD;
+    cell6.innerHTML = buaAn
+    ? `<span class="BuaAnSKXong">Đã Xong</span>`
+    : `<span class="BuaAnSKCXong">Chưa xong</span>`;
+    cell7.innerHTML = `<button class="btnGHICHUSK"><i class="fa-solid fa-edit"></i></button>`;
+}
