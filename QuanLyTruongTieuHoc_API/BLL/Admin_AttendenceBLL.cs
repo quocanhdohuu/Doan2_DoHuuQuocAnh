80e8bdcb413124237fea725afb48b1fd2f174ce5
quocanhvn <quocanhdohuu@gmail.com>
2025-12-28 19:32:16 +0700

    Xay dung API Lay ty le diem danh

---
+++ QuanLyTruongTieuHoc_API/BLL/Admin_AttendenceBLL.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using Models;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class Admin_AttendenceBLL
+    {
+        private readonly Admin_AttendenceDAL _dal;
+
+        public Admin_AttendenceBLL(Admin_AttendenceDAL dal)
+        {
+            _dal = dal;
+        }
+        public Manage_Attendence GetTodayAttendanceSummary(out string error)
+        {
+            return _dal.GetTodayAttendanceSummary(out error);
+        }
+
+    }
+}
