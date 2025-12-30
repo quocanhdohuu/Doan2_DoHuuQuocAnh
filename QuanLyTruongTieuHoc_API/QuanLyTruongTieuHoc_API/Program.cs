b37d20ded72a19e13d50be64a49bea17f591ff5d
quocanhvn <quocanhdohuu@gmail.com>
2025-12-30 09:14:36 +0700

    Xu ly Login tren FE

--- QuanLyTruongTieuHoc_API/QuanLyTruongTieuHoc_API/Program.cs
+++ QuanLyTruongTieuHoc_API/QuanLyTruongTieuHoc_API/Program.cs
@@ -7,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<DatabaseHelper>();
 builder.Services.AddScoped<Admin_UsersDAL>();
@@ -38,9 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
